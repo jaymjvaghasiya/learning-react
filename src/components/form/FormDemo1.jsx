55298d4f0811ab6401d243a84cec58b7aab55b96
Jaykumar Vaghasiya <200120116041@git.org.in>
2025-05-20 08:49:18 +0530

    third commit

--- src/components/form/FormDemo1.jsx
+++ src/components/form/FormDemo1.jsx
@@ -3,14 +3,53 @@ import { useForm } from 'react-hook-form'
 
 export const FormDemo1 = () => {
 
-    const {register, handleSubmit} = useForm();
+    const {register, handleSubmit, formState: {errors}} = useForm();
     const [output, setOutput] = useState({})
     const [isSubmited, setIsSubmited] = useState(false);
 
     const submitHandler = (data) => {
-        // console.log(data);
+        console.log(data);
         setOutput(data);
         setIsSubmited(true);
+    }
+
+    const validationSchema = {
+        name: {
+            required: {
+                value: true,
+                message: "Name is required."
+            }
+        },
+        age: {
+            required: {
+                value: true,
+                message: "Age is required."
+            },
+            validate: (value) => {
+                console.log(value);
+                return value >= 18 || "Minorities re not allowed here."
+            }
+        },
+        contace: {
+            required: {
+                value: true,
+                message: "contact is required."
+            },
+            pattern: {
+                value: /^[6-9]\d{9}$/,
+                message: "Invalide format"
+            }
+        },
+        hobbies: {
+            required: {
+                value: true,
+                message: "hobbies are required."
+            },
+            validate: (value) => {
+                console.log("value...", value);
+                return value?.length >= 2 || "Minimum 2 cities are required."
+            }
+        }
     }
 
   return (
@@ -19,11 +58,18 @@ export const FormDemo1 = () => {
         <form onSubmit={handleSubmit(submitHandler)}>
             <div>
                 <label>Name : </label>
-                <input type='text' {...register("name")} />
+                <input type='text' {...register("name", validationSchema.name)} />
+                <span>{errors.name?.message}</span>
             </div>
             <div>
                 <label>Age : </label>
-                <input type='text' {...register("age")} />
+                <input type='text' {...register("age", validationSchema.age)} />
+                <spa>{errors.age?.message}</spa>
+            </div>
+            <div>
+                <label>Contact : </label>
+                <input type='text' {...register("contact", validationSchema.contace)} />
+                <spa>{errors.contact?.message}</spa>
             </div>
             <div>
                 <label>Gender</label> <br/>
@@ -32,9 +78,10 @@ export const FormDemo1 = () => {
             </div>
             <div>
                 <label>Hbbies : </label> <br/>
-                Cricket <input type='checkbox' value="cricket" {...register("hobbies")} /> <br/>
-                Swimming <input type='checkbox' value="swimming" {...register("hobbies")} /> <br/>
-                Dancing <input type='checkbox' value="dancing" {...register("hobbies")} /> <br/>
+                Cricket <input type='checkbox' value="cricket" {...register("hobbies", validationSchema.hobbies)} /> <br/>
+                Swimming <input type='checkbox' value="swimming" {...register("hobbies", validationSchema.hobbies)} /> <br/>
+                Dancing <input type='checkbox' value="dancing" {...register("hobbies", validationSchema.hobbies)} /> <br/>
+                <spa>{errors.hobbies?.message}</spa>
             </div>
             <div>
                 <label>Country</label>
@@ -56,6 +103,7 @@ export const FormDemo1 = () => {
             isSubmited && <div style={{color: output.color}}><br/>
                     Name : {output.name} <br/>
                     Age : {output.age} <br/>
+                    Age : {output.contact} <br/>
                     gender : {output.gender} <br/>
                     Country : {output.country} <br/>
                     Hobbies :- <br/>
